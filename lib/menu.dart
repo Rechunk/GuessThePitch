b0a3077259350f07365682447329e701b8ad9d1e
Rechunk <gabri.marc@gmx.net>
2017-06-05 14:04:48 +0200

    Menu improved visually

--- lib/menu.dart
+++ lib/menu.dart
@@ -20,29 +20,50 @@ class _Menu extends State<Menu> {
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            new Text("Soundy", style: new TextStyle(fontSize: 60.0, height: 2.0, letterSpacing: 5.0, color: new Color(0xFF95989A))),
-            new RaisedButton(color: Colors.transparent, elevation: 0.0, highlightElevation: 0.0, splashColor: Colors.red, child: new Text("Play", style: new TextStyle(fontSize: 20.0)), onPressed: (){
+            new Text("GUESS THE", style: new TextStyle(fontFamily: "Coiny", fontSize: 45.0, color: new Color(0xFF7F4937))),
+            new Text("PITCH", style: new TextStyle(fontFamily: "Coiny", fontSize: 83.0, height: 0.1, color: mainColor)),
+            new Container(
+              margin: new EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
+              child: new Column(
+                children: [
+                  new Padding(
+                    padding: new EdgeInsets.all(13.0),
+                    child: new RaisedButton(color: Colors.transparent, elevation: 0.0, highlightElevation: 0.0, splashColor: Colors.red, child: new Text("Play", style: new TextStyle(fontSize: 23.0, fontFamily: "Quicksand")), onPressed: (){
+                      Navigator.push(context, new CustomRoute(
+                          rightToLeft,
+                          builder: (_) => new Grid()
+                      ));
+                    }),
+                  ),
+                  new Padding(
+                    padding: new EdgeInsets.all(13.0),
+                    child: new RaisedButton(color: Colors.transparent, elevation: 0.0, highlightElevation: 0.0, splashColor: Colors.red, child: new Text("Options", style: new TextStyle(fontSize: 23.0, fontFamily: "Quicksand")), onPressed: (){
+                      Navigator.push(context, new CustomRoute(
+                          leftToRight,
+                          builder: (_) => new Options()
+                      ));
+                    }),
+                  ),
+                  new Padding(
+                    padding: new EdgeInsets.all(13.0),
+                    child: new RaisedButton(color: Colors.transparent, elevation: 0.0, highlightElevation: 0.0, splashColor: Colors.red, child: new Text("Help", style: new TextStyle(fontSize: 23.0, fontFamily: "Quicksand")), onPressed: (){
+                      Navigator.push(context, new CustomRoute(
+                          leftToRight,
+                          builder: (_) => new Help()
+                      ));
+                    }),
+                  ),
+                  new Padding(
+                    padding: new EdgeInsets.all(13.0),
+                    child: new RaisedButton(color: Colors.transparent, elevation: 0.0, highlightElevation: 0.0, splashColor: Colors.red, child: new Text("Exit", style: new TextStyle(fontSize: 23.0, fontFamily: "Quicksand")), onPressed: (){
 
-              Navigator.push(context, new CustomRoute(
-                  rightToLeft,
-                  builder: (_) => new Grid()
-              ));
-            }),
-            new RaisedButton(color: Colors.transparent, elevation: 0.0, highlightElevation: 0.0, splashColor: Colors.red, child: new Text("Options", style: new TextStyle(fontSize: 20.0)), onPressed: (){
-              Navigator.push(context, new CustomRoute(
-                  leftToRight,
-                  builder: (_) => new Options()
-              ));
-            }),
-            new RaisedButton(color: Colors.transparent, elevation: 0.0, highlightElevation: 0.0, splashColor: Colors.red, child: new Text("Help", style: new TextStyle(fontSize: 20.0)), onPressed: (){
-              Navigator.push(context, new CustomRoute(
-                  leftToRight,
-                  builder: (_) => new Help()
-              ));
-            }),
-            new RaisedButton(color: Colors.transparent, elevation: 0.0, highlightElevation: 0.0, splashColor: Colors.red, child: new Text("Exit", style: new TextStyle(fontSize: 20.0)), onPressed: (){
+                    }),
+                  ),
+                ]
+              ),
+            )
 
-            }),
+
           ]
         ),
       ),
