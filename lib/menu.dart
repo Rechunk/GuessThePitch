b073750d7b453764944f036b9a15fea89c38f37f
Rechunk <gabri.marc@gmx.net>
2017-06-07 16:21:30 +0200

    Options button removed (not needed yet)

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import "options.dart";
 import "route.dart";
 import "game.dart";
 import "config.dart";
@@ -11,8 +10,8 @@ class Menu extends StatefulWidget {
   @override
   _Menu createState() => new _Menu();
 }
-int highscore = 0;
 
+int highscore = 0;
 
 class _Menu extends State<Menu> {
 
@@ -49,15 +48,6 @@ class _Menu extends State<Menu> {
                       Navigator.push(context, new CustomRoute(
                           navigationDirection: rightToLeft,
                           builder: (_) => new Game()
-                      ));
-                    }),
-                  ),
-                  new Padding(
-                    padding: new EdgeInsets.all(13.0),
-                    child: new RaisedButton(color: Colors.transparent, elevation: 0.0, highlightElevation: 0.0, splashColor: Colors.red, child: new Text("Options", style: new TextStyle(fontSize: 23.0, fontFamily: "Quicksand")), onPressed: (){
-                      Navigator.push(context, new CustomRoute(
-                          navigationDirection: leftToRight,
-                          builder: (_) => new Options()
                       ));
                     }),
                   ),
