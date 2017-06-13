ab4368b36e02edcbe6243c82d3812d48d4eca684
Rechunk <gabri.marc@gmx.net>
2017-06-13 15:48:58 +0200

    Counter widget seperated into it's own file

---
+++ lib/score_counter.dart
@@ -0,0 +1,31 @@
+import "package:flutter/material.dart";
+import "game.dart";
+
+class ScoreCounter extends StatefulWidget {
+  @override
+  _ScoreCounter createState() => new _ScoreCounter();
+}
+
+class _ScoreCounter extends State<ScoreCounter> with SingleTickerProviderStateMixin{
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..forward();
+  }
+
+  @override
+  build(BuildContext context){
+    return new AnimatedBuilder(
+      animation: _controller,
+      child: new Container(width: 200.0, height: 200.0, color: Colors.green),
+      builder: (BuildContext context, Widget child) {
+        return new Text("$score", style: new TextStyle(fontFamily: "Coiny", fontSize: 60.0, color: mainColor));
+      },
+    );
+  }
+}
